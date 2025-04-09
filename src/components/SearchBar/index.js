ae32df756a939f353d829fa6c1f2387b02a3bf32
Jeffrin Rijo V C <jeffrinrijo4@gmail.com>
2025-04-09 08:09:48 +0530

    Added Search Bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,11 +1,66 @@
 import * as React from 'react';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+
 
 export default function SearchBar() {
 
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        padding: 0,
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha("#1D2537", 0.65),
+        '&:hover': {
+            backgroundColor: alpha("#1D2537", 1),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '60%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+        },
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }));
+
+    const SearchIconWrapper = styled('button')(({ theme }) => ({
+        padding: 0,
+        background: 'none',
+        border: 'none',
+        outline: 'none',
+        boxShadow: 'none',    
+        backgroundColor: "#4F4E4C",
+        color: '#fff',
+        height: '100%',
+        width: '5%',
+        pointerEvents: 'none'
+    }));
+
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            [theme.breakpoints.up('md')]: {
+                width: '20ch',
+            },
+        },
+    }));
+
 
     return (
-        <div>
-            SearchBar
-        </div>
+        <Search>
+            <StyledInputBase
+                placeholder="Enter github username"
+                inputProps={{ 'aria-label': 'search' }}
+            />
+            <SearchIconWrapper>
+                <SearchIcon />
+            </SearchIconWrapper>
+        </Search>
     );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,12 @@
 const react = require('react')
 const { useState, useEffect } = require('react')
+import SearchBar from '../../components/SearchBar';
 
 export default function HomePage() {
     const [loading, setLoading] = useState(true);
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+            <SearchBar />
         </div>
     )
 }
